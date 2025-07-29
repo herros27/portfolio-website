7deb18d0b25a728548faf7d840735cea458700f8
Kemas Khairunsyah <khairunsyah2714@gmail.com>
2025-07-30 03:34:38 +0700

    Edit sesuatu

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,13 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        <span className="font-bold">Hello, I'm Kemas Khairunsyah.</span> I'm a{" "}
+        <span className="font-bold">freelance developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">React (Next.js){" "}</span>.
+        <span className="underline">, Laravel, And {" "}</span>.
+        <span className="underline">Mobile (Flutter & Kotlin){" "}</span>.
       </motion.h1>
 
       <motion.div
@@ -100,7 +102,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/kemaskhairunsyah/?locale=in_ID"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +110,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/herros27"
           target="_blank"
         >
           <FaGithubSquare />
